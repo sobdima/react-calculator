da28cc0f562ac5f67873431f5179310853bf616d
Dima Sob <sobolevskii.dmit@gmail.com>
2025-08-19 00:15:15 +0200

    feat: add keyPress functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import { useState } from 'react';
+//РАЗОБРАТЬСЯ С ПРЕДУПРЕЖДЕНИЕМ в useCallback
+//ВЫНЕСТИ ФУНКЦИИ В utils   по типу такого => utils/handleKeyPress.tsx OR utils/functions.tsx
+//ПОПРОБОВАТЬ ПОИСКАТЬ ОПТИМИЗАЦИЮ ПО РЕНДЕРИНГУ ВСЕХ КОПМОНЕНТОВ </Cell> - МОЖЕТ МОЖНО НАЙТИ СПОСОБ НЕ ПРОПИСЫВАТЬ ИХ СТОЛЬКО РАЗ ПОДРЯД, МОЖЕТ МОЖНО ПРИДУМАТЬ КАКОЙ-ТО ЦИКЛ
+//ПОСМОТРЕТЬ МОЖЕТ В ЭТОЙ ФУНКЦИИ onClick={() => handleClick('1')} МОЖНО ПЕРЕДАВАТЬ НЕ "1", КАК МАГИЧЕСКОЕ ЧИСЛО, А КОНКРЕТНО innerText САМОГО ЭЛЕМЕНТА (ЧЕРЕЗ target ???)
+//МОЖНО ДОБАВИТЬ АНИМАЦИЮ КНОПОК - ТИПА КЛИКНУЛ ПО КНОПКЕ, И ОНА НА ПОЛСЕКУНДЫ ИЗМЕНИЛА ЦВЕТ НА ЖЕЛТЕНКИЙ ИЛИ СЕРЕНЬКИЙ
+//ДЕПЛОЙ НА НЕТЛИФАЙ + СДЕЛАТЬ README.MD ФАЙЛ ДЛЯ ГИТХАБА С ОПИСАНИЕМ ПРОЕКТА
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 import Cell from './components/Cell';
 import { evaluate } from 'mathjs';
@@ -22,13 +28,59 @@ function App() {
 
   function handleClick(value: string) {
     if (value === '=') {
+      console.log('Get an expression: ', display);
       const expression = display.replace(/[+\-*/]+$/g, '');
+      console.log('Get a trimmed expression: ', expression);
       setDisplay(evaluate(expression).toString());
       return;
     }
 
     setDisplay((prev) => (prev === '0' ? value : prev + value));
   }
+
+  const handleKeyPress = useCallback(
+    (event: KeyboardEvent) => {
+      const allowKeys = [
+        '0',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        ',',
+        '.',
+        '=',
+        '+',
+        '-',
+        '*',
+        '/',
+      ];
+
+      const { key } = event;
+      let value = key;
+
+      if (key === 'Enter') value = '=';
+      if (key === 'Backspace') setDisplay('0');
+
+      if (allowKeys.includes(value)) {
+        console.log('Pressed: ', value);
+        handleClick(value);
+      }
+    },
+    [display] //РАЗОБРАТЬСЯ С ЭТИМ ПРЕДУПРЕЖДЕНИЕМ
+  );
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [handleKeyPress]);
 
   return (
     <>
